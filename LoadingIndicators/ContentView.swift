c2f9f13ace90ca9496650ecb7567abc5c4a8f360
JagCesar <cesar@jagcesar.se>
2019-12-29 20:06:43 +0100

    Create project

---
+++ LoadingIndicators/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoadingIndicators
+//
+//  Created by jagcesar on 2019-12-29.
+//  Copyright © 2019 Ambi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
